267e35a41d6d1c4bc69e6919acaa7caaf45e4a4d
Daniel Cook <daniel@danielcook.me>
2017-05-21 23:38:21 -0230

    Added copyright notice to superxbr

--- src/postprocess/superxbr.rs
+++ src/postprocess/superxbr.rs
@@ -1,3 +1,34 @@
+/*
+
+Rust port of Hyllian's original code at http://pastebin.com/cbH8ZQQT.
+Original license/copyright notice below.
+
+*******  Super XBR Scaler  *******
+
+
+
+Copyright (c) 2016 Hyllian - sergiogdb@gmail.com
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+
+*/
+
 macro_rules! red {
     ($x:expr) => (($x >> 0) & 0xff)
 }
